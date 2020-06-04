d9fdfdf1fd2eec07e177c073b3e09ff3445a3afd
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-06-04 17:36:09 +0000

    Incremental updates to direct testing

--- testing/direct_call/0_start_docker.sh
+++ testing/direct_call/0_start_docker.sh
@@ -3,8 +3,9 @@
 # the start_docker.sh script in /docker
 
 DATAD="/home/mwyczalk_test/Projects/GermlineCaller/C3L-00001"
+REFD="/diskmnt/Datasets/Reference"
 source ../../docker/docker_image.sh
 
-cd ../.. && bash docker/WUDocker/start_docker.sh $@ -I $IMAGE $DATAD:/data
+cd ../.. && bash docker/WUDocker/start_docker.sh $@ -I $IMAGE $DATAD:/data $REFD:/Reference
 
 
