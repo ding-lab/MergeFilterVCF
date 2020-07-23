69f8212204d697bee8c39fa22a1c70519b0afcea
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-07-23 16:57:12 -0500

    Bugfix to docker start

--- testing/direct_call/0b_start_docker.filter.sh
+++ testing/direct_call/0b_start_docker.filter.sh
@@ -1,16 +1,16 @@
-# start docker image with ../demo_data mapped to /data,
-# unless another path is passed on command line.  uses
-# the start_docker.sh script in /docker
 
 #DATAD="/home/mwyczalk_test/Projects/GermlineCaller/C3L-00001"
 DATAD="/home/mwyczalk_test/Projects/TinDaisy/testing/C3L-00908-data/dat"
 REFD="/diskmnt/Datasets/Reference"
-OUTD="./results"
+
+# changing directories so entire project directory is mapped by default
+cd ../..
+OUTD="testing/direct_call/results"  # output dir relative to ../..
 mkdir -p $OUTD
 
-source ../../docker/docker_image.sh
+source docker/docker_image.sh
 IMAGE=$IMAGE_FILTER
 
-cd ../.. && bash docker/WUDocker/start_docker.sh $@ -I $IMAGE $DATAD:/data $REFD:/Reference $OUTD:/results
+bash docker/WUDocker/start_docker.sh $@ -I $IMAGE $DATAD:/data $REFD:/Reference $OUTD:/results
 
 
